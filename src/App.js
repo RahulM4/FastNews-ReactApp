44a518777f0c50c9c00b72dbef49ee22f42777bc
RAHUL MAHTO <75800571+RahulM4@users.noreply.github.com>
2023-04-28 23:52:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = ()=> {
         <LoadingBar height={3} color='#f11946' progress={progress} />
           <Routes>
            
-            <Route exact path="/"          element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+            <Route exact path="/FastNewsApp"          element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
             <Route exact path="/business"     element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />}/>
             <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />}/> 
             <Route exact path="/general"       element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />}/>
