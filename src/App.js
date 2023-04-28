479361da549932d271ebf2c7ebcc0238d03ab693
RAHUL MAHTO <75800571+RahulM4@users.noreply.github.com>
2023-04-29 00:02:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
@@ -13,12 +13,12 @@ const App = ()=> {
  
     return (
       <div>
-        <Router  basename="/FastNewsApp">
+        <Router  basename="/">
         <NavBar/> 
         <LoadingBar height={3} color='#f11946' progress={progress} />
           <Routes>
            
-            <Route exact path="/"          element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+            <Route  path="/"          element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
             <Route  path="/business"     element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />}/>
             <Route  path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />}/> 
             <Route  path="/general"       element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />}/>
